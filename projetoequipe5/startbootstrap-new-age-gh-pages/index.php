d613c42ca6be64f4600211d6e15bdfab542022eb
Victor Loic Lemos Viana <55466319+Vitu26@users.noreply.github.com>
2022-11-04 08:09:49 -0300

    Add files via upload

---
+++ projetoequipe5/startbootstrap-new-age-gh-pages/index.php
@@ -0,0 +1,55 @@
+<?php
+
+ include_once("templates/header.php");
+
+?>
+
+        <!-- Quote/testimonial aside-->
+        <aside class="text-center bg-gradient-primary-to-secondary">
+            <div class="container px-5">
+                <div class="row gx-5 justify-content-center">
+                    <div class="col-xl-8">
+                            <Form method="POST" action="">
+                            <div class="mb-3">
+                                <h2>Infome seu email para receber atualizações!</h2>
+                                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Digite seu email">
+                            </div>
+                            <button type="submit" class="btn btn-primary">Enviar</button>
+                            </form>
+                    </div>
+                </div>
+            </div>
+        </aside>
+        
+        <!-- Basic features section-->
+        <section class="bg-light">
+            <div class="container px-5">
+                <div class="row gx-5 align-items-center justify-content-center justify-content-lg-between">
+                    
+                        <h2 class="display-4 lh-1 mb-4">O que é o Canvas de Usabilidade?</h2>
+                        <p class="lead fw-normal text-muted mb-5 mb-lg-0">Usabilidade é um atributo de qualidade de software relacionado à facilidade de se utilizá-lo e esse atributo é relevante para vários tipos de sistemas. Para verificar se o produto ou serviço pretendido atende aos atributos de usabilidade exigidos em relação aos usuários esperados, é feita uma avaliação de usabilidade. A avaliação de usabilidade é um nome genérico para um grupo de métodos baseados na avaliação e inspeção ou exame relacionado com aspectos de usabilidade da interface com o usuário. Existem várias alternativas para se realizar a avaliação da usabilidade de uma solução. OCAU (O canvas de Avaliação de Usabilidade) é um artefato desenvolvido com a intenção de facilitar o planejamento da avaliação de usabilidade possuindo 8 dimensões.</p>
+                    
+                </div>
+            </div>
+        </section>
+        <!-- Call to action section-->
+
+        <!-- App badge section-->
+        <section class="bg-gradient-primary-to-secondary" id="download">
+            <div class="container px-5">
+                <h2 class="text-center text-white font-alt mb-4">Nos siga nas redes sociais!</h2>
+                <div class="d-flex flex-column flex-lg-row align-items-center justify-content-center">
+                    <a href="#" class="icon-new" id="insta"><i class="bi bi-instagram"></i></a>
+                    <a href="#" class="icon-new" id="face"><i class="bi bi-facebook"></i></a>
+                    <a href="https://wa.me/5583986627934" class="icon-new" id="wpp"><i class="bi bi-whatsapp"></i></a>
+                </div>
+            </div>
+        </section>
+    
+
+<?php
+
+ include_once("templates/footer.php");
+
+?>
+        
